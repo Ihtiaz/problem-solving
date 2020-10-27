520c7a5287dd75630fde6ccea7c61ca2be79b35f
ihtiaz <ishmamihtiaz@codemarshal.com>
2020-10-28 00:05:33 +0600

    feat(cf-1437a): added cpp solution to codeforces problem marketing scheme

---
+++ codeforces/cpp/Marketing Scheme (1437A).cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pii pair <int,int>
+#define ff first
+#define ss second
+#define pi acos(-1.0)
+#define pb push_back
+#define INF (ll)1e18
+#define N 200002
+#define MOD 1000000007
+#define BASE 100003
+const double EPS = 1e-9;
+#define eq(a,b) (fabs(a-b)<EPS)
+ 
+#define FastRead ios_base::sync_with_stdio(0);cin.tie(0)
+using namespace std;
+ 
+template < class T > inline T gcd(T a, T b) {
+    while(b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+template < class T > inline T lcm(T a, T b) {return a/gcd(a, b)*b;}
+inline int nxt() {int wow; scanf("%d", &wow); return wow;}
+inline ll lxt() {ll wow; scanf("%lld", &wow); return wow;}
+inline double dxt() {double wow; scanf("%lf", &wow); return wow;}
+int bitXor(int x, int y){return ~( (x & y) | (~x & ~y) );}
+template < class T > inline T bigmod(T p, T e, T M){
+    ll ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return (T)ret;
+}
+/// send ll as arguments
+ 
+ 
+/***************** Amakakeru Ryu No Hirameki *******************/
+ 
+ 
+int main() {
+    //freopen("in.txt", "r", stdin);
+    int t = nxt();
+    while (t--) {
+        int x = nxt(), y = nxt();
+        if (y < x*2) puts("YES");
+        else puts("NO");
+    }
+    return 0;
+}
